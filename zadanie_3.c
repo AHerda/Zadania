d977a6956d9ade716d7a01a2817abcef2b0a8b68
AdiKozak <92255793+AdiKozak@users.noreply.github.com>
2021-11-02 21:37:25 +0100

    Update zadanie_3.c

--- zadanie_3.c
+++ zadanie_3.c
@@ -2,12 +2,23 @@
 
 int main()
 {
-	float n=1;
+	float n=0;
 	float suma=0;
 	while(suma<=10)
 	{
-		suma+=(1/n);
 		n+=1;
+		suma+=(1/n);
+/*niezmiennik:
+suma=1+1/2+1/3+...+1/n
+dowód:
+
+1. n=0 => suma_0=0
+2. zał. ind.:
+	dla dowolnego n
+	suma_n=1+1/2+...+1/n
+		
+	suma_(n+1)=suma_n+1/(n+1)=(zał.ind.) 1+1/2+1/3+...+1/(n+1)
+Na mocy zasady indukcji teza jest prawdziwa*/
 	}
 	printf("najmniejsza liczba spełniająca nierówność to: %f\n", n);
 	return 0;
