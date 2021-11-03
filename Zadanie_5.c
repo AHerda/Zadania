ef07eededf07c22e4793305ba9c8de84c2a6f57d
AdiKozak <92255793+AdiKozak@users.noreply.github.com>
2021-11-03 11:03:13 +0100

    Create Zadanie_5.c

---
+++ Zadanie_5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int NWD(int a, int b)
+{
+	if(b!=0)
+		return NWD(b, a%b);
+	return a;
+}
+
+int main()
+{
+	float ilosc=0;
+	for(int a=1; a<=1000; a++)
+	{
+		for(int b=1; 1<=a; b++)
+		{
+			if(NWD(a, b)==1)
+			{
+				if(a!=b)
+					ilosc+=2;
+				else
+					ilosc+=1;
+			}
+		}
+		printf("%d;%f\n", a, ilosc/(a**2));
+	}
+	return 0;
+}
